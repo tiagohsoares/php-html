82ee4f4c2df7e410d697d3526555f96f06be9f3e
tiagohsoares <tiagowenceslau11@gmail.com>
2025-05-08 19:12:05 -0300

    index livros

--- index.php
+++ index.php
@@ -3,9 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Testes</title>
+    <title>Document</title>
 </head>
 <body>
-    
+        <?php
+            $livros = [
+                [
+                    "nome"=> "Morro dos ventos Uivantes",
+                    "autor"=> "J. K. Rowling",
+                    "ano" => "1975",
+                    "purchaseUrl"=> "https:\\lojaA.com.br",
+                ],
+                [
+                    "nome" => "Harry Potter",
+                    "autor" => "J. K. Rowling",
+                    "ano" => "1973",
+                    "purchaseUrl" => "https:\\lojaB.com.br",
+                ]
+
+            ];
+        ?>
+
+         <ul>
+            <h1>
+            <?php foreach ($livros as $livro){
+                echo "<a href='{$livro['purchaseUrl']}'>{$livro['nome']};</a><br>";
+            }
+            ?>
+         </ul>              
 </body>
 </html>
